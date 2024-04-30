9625720e967c62acf69c14ae097aac7c18ea3648
Cartodynamics <59633245+Cartodynamics@users.noreply.github.com>
2024-04-29 20:39:40 -0600

    Delete mapa_interactivo_tijuana_paco_torres_ok directory

--- mapa_interactivo_tijuana_paco_torres_ok/data/UABC_11.js
+++
@@ -1,1 +0,0 @@
-var json_UABC_11 = {"type":"FeatureCollection","name":"UABC_11","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"latitude":null,"longitude":null,"label":"UABC"},"geometry":{"type":"Point","coordinates":[-116.968161629931501,32.536867875491772]}}]}
